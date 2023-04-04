a7e40f5451ca52856e80e74d374955e8283b20b5
Batuhan TATLISERT <90261616+Batuhugu@users.noreply.github.com>
2023-04-04 17:09:09 +0300

    Initial commit

---
+++ Assets/Scripts/Not Used/ColorPalette.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "Create ColorPalette", fileName = "ColorPalette", order = 0)]
+public class ColorPalette : ScriptableObject
+{
+    public Color background;
+    //public Color snake;
+}
